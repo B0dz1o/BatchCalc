6819f989fc9d0a4016feb42b0c90da946c949371
Piotr Bogdan <bogdzio@o2.pl>
2016-09-06 19:20:04 +0200

    Base project

---
+++ Batch_Calc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Batch_Calc
+//
+//  Created by Piotr Bogdan on 9/6/16.
+//  Copyright © 2016 Piotr Bogdan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
