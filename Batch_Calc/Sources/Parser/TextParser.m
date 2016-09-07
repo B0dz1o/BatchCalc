71584153d694451fa451dde4b0216bd96be77c06
Piotr Bogdan <bogdzio@o2.pl>
2016-09-07 18:38:29 +0200

    Text parsing with regex

--- Batch_Calc/Sources/Parser/TextParser.m
+++ Batch_Calc/Sources/Parser/TextParser.m
@@ -22,7 +22,33 @@ NSArray <NSString *> * operations;
 }
 
 -(BOOL) checkBatch {
+    NSUInteger countOps = [operations count] - 1;
+    NSString * pattern = @"^(add|multiply|power|divide|subtract) \\d+\\.?\\d+?$";
+    for (NSUInteger i = 0; i < countOps ; ++i) {
+        NSString * line = [operations objectAtIndex:i];
+        if (![self checkPattern: pattern forLine: line]) {
+            return false;
+        }
+    }
+    NSString * patternLast = @"^apply \\d+\\.?\\d+?$";
+    NSString * lineLast = [operations lastObject];
+    if (![self checkPattern:patternLast forLine:lineLast]) {
+        return false;
+    }
     return true;
+}
+
+-(BOOL) checkPattern: (NSString *) pattern forLine: (NSString *) line {
+    NSRegularExpression * opRegex = [NSRegularExpression
+                                     regularExpressionWithPattern:pattern
+                                     options:NSRegularExpressionAnchorsMatchLines error:nil];
+    if (1 == [opRegex numberOfMatchesInString:line
+                                      options:NSMatchingWithoutAnchoringBounds
+                                        range:NSMakeRange(0, [line length])]) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 -(void) performOperations {
